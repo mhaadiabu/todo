a1309fa0bd607e447a01dd5b14f932d1e875ee87
mhaadiabu <mhabubakar999@outlook.com>
2025-08-05 14:08:50 +0000

    feat: implement search functionality in Todo list and remove SearchTodo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,54 @@
-import { useReducer } from 'react';
+import { useMemo, useState } from 'react';
 
 import Todo from './components/Todo';
-import SearchTodo from './components/SearchTodo';
 import NewTodo from './components/NewTodo';
 
-import { reducer } from './lib/utils';
+import { useTodo } from './context/TodoContext';
 
 const App = () => {
-	const [todos, dispatch] = useReducer(reducer, []);
+	const [search, setSearch] = useState('');
+
+	const { todos } = useTodo();
+
+	const filteredTodos = useMemo(() => {
+		return todos.filter((todo) => {
+			return todo.task.toLowerCase().includes(search.toLowerCase());
+		});
+	}, [todos, search]);
+
+	console.log(filteredTodos);
 
 	return (
 		<main className='w-full min-h-screen flex flex-col justify-center items-center bg-neutral-50 px-4 sm:px-6'>
 			<div className='max-w-4xl w-full flex flex-1 flex-col justify-start mx-auto'>
-				<h1 className='text-3xl font-bold text-center my-6'>ToDo List</h1>
-				<SearchTodo />
-				{todos.map((todo) => (
+				<h1 className='text-3xl font-bold text-center my-6 capitalize'>
+					ToDo List
+				</h1>
+				<div className='input flex mx-0'>
+					<input
+						name='search-input'
+						type='text'
+						placeholder='Search note...'
+						className='h-full w-full outline-none border-none focus:border-2'
+						autoComplete='off'
+						value={search}
+						onChange={(e) => setSearch(e.target.value)}
+					/>
+					<button
+						onClick={() => setSearch('')}
+						className={`justify-end items-center w-6 h-6 ${
+							search.length > 0 ? 'flex' : 'hidden'
+						}`}>
+						X
+					</button>
+				</div>
+				{filteredTodos.map((todo) => (
 					<Todo
 						key={todo.id}
-						dispatch={dispatch}
 						todo={todo}
 					/>
 				))}
-				<NewTodo dispatch={dispatch} />
+				<NewTodo />
 			</div>
 		</main>
 	);
